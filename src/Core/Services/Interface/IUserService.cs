e535f5be11e976bbe48a13b77b09d29459382026
abas1200 <abas.olfat@yahoo.com>
2024-11-18 18:59:29 -0500

    added core and common projects

---
+++ src/Core/Services/Interface/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using PPT.App.Common.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPT.App.Core.Services.Interface
+{
+    public class IUserService : IScopeDependency
+    {
+    }
+}
