65f0df5746dfba5ca81c84cb32f366e753bc1dcc
abas1200 <abas.olfat@yahoo.com>
2024-11-19 12:20:31 -0500

    added more test

--- tests/UserApi/UserServiceTests.cs
+++ tests/UserApi/UserServiceTests.cs
@@ -21,7 +21,22 @@ namespace PPT.Test.UserServiceTests
 
             Assert.IsNotNull(result);
             Assert.That(result, Is.EqualTo("https://api.dicebear.com/8.x/pixel-art/png?seed=7&size=150"));
-             
+
+
+            var result2 = await _userService.GetUserAvatarAsync("userttt@e");
+
+            Assert.IsNotNull(result2);
+            Assert.That(result2, Is.EqualTo("https://api.dicebear.com/8.x/pixel-art/png?seed=e&size=150"));
+
+            var result3 = await _userService.GetUserAvatarAsync("userttt@w");
+
+            Assert.IsNotNull(result3);
+            Assert.IsTrue((new List<string>{ "https://api.dicebear.com/8.x/pixel-art/png?seed=1&size=150", 
+                "https://api.dicebear.com/8.x/pixel-art/png?seed=2&size=150", 
+                "https://api.dicebear.com/8.x/pixel-art/png?seed=3&size=150",
+                "https://api.dicebear.com/8.x/pixel-art/png?seed=4&size=150",
+                "https://api.dicebear.com/8.x/pixel-art/png?seed=5&size=150"}).Contains(result3));
+
 
         }
     }
